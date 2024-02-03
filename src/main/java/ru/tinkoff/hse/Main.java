c43079ce15c1228b5e8400972657773f974a99b5
Artem <avdmitriev_2@edu.hse.ru>
2024-02-03 19:54:43 +0300

    [HW1] P2

--- src/main/java/ru/tinkoff/hse/Main.java
+++ src/main/java/ru/tinkoff/hse/Main.java
@@ -8,6 +8,7 @@ public class Main {
             return;
         }
 
+        // comment
         try {
             int num1 = Integer.parseInt(args[0]);
             int num2 = Integer.parseInt(args[1]);
