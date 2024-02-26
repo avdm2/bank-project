898fad203a90b2ac021807a94a00c3750dfe2c91
Artem <avdmitriev_2@edu.hse.ru>
2024-02-26 15:29:13 +0300

    [HW3] Minor changes

--- src/main/java/ru/tinkoff/hse/exceptions/GlobalHandler.java
+++ src/main/java/ru/tinkoff/hse/exceptions/GlobalHandler.java
@@ -6,17 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.net.ConnectException;
-
 @ControllerAdvice
 public class GlobalHandler {
-
-    @ExceptionHandler(ConnectException.class)
-    public ResponseEntity<String> handleConnectionException(ConnectException exception) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(exception.getMessage());
-    }
 
     @ExceptionHandler(JsonProcessingException.class)
     public ResponseEntity<String> handleJsonProcessingException(JsonProcessingException exception) {
