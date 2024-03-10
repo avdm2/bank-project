5f6ad09b4bf40f6ee7e6310a6ae0d691f32cb57f
Artem <avdmitriev_2@edu.hse.ru>
2024-03-10 16:05:25 +0300

    [HW4] Minor fixes

--- accounts/src/main/java/ru/tinkoff/hse/dto/GetAccountResponse.java
+++ accounts/src/main/java/ru/tinkoff/hse/dto/GetAccountResponse.java
@@ -1,11 +1,13 @@
 package ru.tinkoff.hse.dto;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 
 @Setter
+@Getter
 @Accessors(chain = true)
 public class GetAccountResponse {
 
