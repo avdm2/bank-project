ff0a28be46666f2767c3ce4f1e54bff741fc3368
Artem <avdmitriev_2@edu.hse.ru>
2024-03-14 20:45:45 +0300

    [HW4] fix

--- accounts/src/main/java/ru/tinkoff/hse/services/KeycloakTokenRequestService.java
+++ accounts/src/main/java/ru/tinkoff/hse/services/KeycloakTokenRequestService.java
@@ -3,12 +3,17 @@ package ru.tinkoff.hse.services;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.endpoint.InvalidEndpointRequestException;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import ru.tinkoff.hse.dto.KeycloakTokenRequest;
 import ru.tinkoff.hse.dto.KeycloakTokenResponse;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 @Service
 public class KeycloakTokenRequestService {
@@ -26,14 +31,23 @@ public class KeycloakTokenRequestService {
     private String clientId;
 
     public String getToken() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HashMap<String, String> body = new LinkedHashMap<>();
+        body.put("client_id", clientId);
+        body.put("client_secret", clientSecret);
+        body.put("grant_type", "urn:ietf:params:oauth:grant-type:token-exchange");
+
+        HttpEntity<HashMap<String, String>> entity = new HttpEntity<>(body, headers);
+
         ResponseEntity<KeycloakTokenResponse> response = new RestTemplateBuilder()
                 .setConnectTimeout(Duration.ofSeconds(10))
                 .setReadTimeout(Duration.ofSeconds(10))
                 .build()
-                .postForEntity(keycloakUrl + "/realms/" + keycloakRealm + "/protocol/openid-connect/token",
-                        new KeycloakTokenRequest()
-                                .setClientId(clientId)
-                                .setClientSecret(clientSecret),
+                .exchange(keycloakUrl + "/realms/" + keycloakRealm + "/protocol/openid-connect/token",
+                        HttpMethod.POST,
+                        entity,
                         KeycloakTokenResponse.class);
         if (!response.getStatusCode().is2xxSuccessful()) {
             throw new InvalidEndpointRequestException(
