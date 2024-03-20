d22ad58254685a7673dee9a56dde22ce84663166
Artem <avdmitriev_2@edu.hse.ru>
2024-03-20 12:47:59 +0300

    [HW5] fixes

--- accounts/src/main/java/ru/tinkoff/hse/configurations/GrpcConfiguration.java
+++ accounts/src/main/java/ru/tinkoff/hse/configurations/GrpcConfiguration.java
@@ -1,21 +1,17 @@
 package ru.tinkoff.hse.configurations;
 
-import io.grpc.Channel;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import net.devh.boot.grpc.client.inject.GrpcClientBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.tinkoff.hse.lib.CurrencyConverterGrpc;
 
 @Configuration(proxyBeanMethods = false)
-
+@GrpcClientBean(
+        clazz = CurrencyConverterGrpc.CurrencyConverterBlockingStub.class,
+        beanName = "converterStub",
+        client = @GrpcClient(
+                value = "grpcConverterClientService"
+        )
+)
 public class GrpcConfiguration {
-
-        @GrpcClient("converter")
-        private Channel channel;
-
-        @Bean
-        public CurrencyConverterGrpc.CurrencyConverterBlockingStub currencyConverterBlockingStub() {
-                return CurrencyConverterGrpc.newBlockingStub(channel);
-        }
 }
