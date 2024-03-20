7b6b39d6818e6443a5a63e3543c64a682f50adb2
Artem <avdmitriev_2@edu.hse.ru>
2024-03-20 20:17:09 +0300

    [HW5] fix

--- accounts/src/main/java/ru/tinkoff/hse/configurations/GrpcConfiguration.java
+++ accounts/src/main/java/ru/tinkoff/hse/configurations/GrpcConfiguration.java
@@ -2,11 +2,8 @@ package ru.tinkoff.hse.configurations;
 
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import net.devh.boot.grpc.client.inject.GrpcClientBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.tinkoff.hse.lib.CurrencyConverterGrpc.CurrencyConverterBlockingStub;
-import ru.tinkoff.hse.services.GrpcConverterClientService;
 
 @Configuration(proxyBeanMethods = false)
 @GrpcClientBean(
@@ -17,9 +14,4 @@ import ru.tinkoff.hse.services.GrpcConverterClientService;
         )
 )
 public class GrpcConfiguration {
-
-    @Bean
-    public GrpcConverterClientService grpcConverterClientService(@Autowired CurrencyConverterBlockingStub converterStub) {
-        return new GrpcConverterClientService(converterStub);
-    }
 }
