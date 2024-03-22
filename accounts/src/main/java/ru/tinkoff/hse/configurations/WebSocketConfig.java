49f4bba4f525062252a944ce26e4f87c9e59bcf0
Artem <avdmitriev_2@edu.hse.ru>
2024-03-22 20:11:29 +0300

    [HW5] fix

--- accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
+++ accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
@@ -19,5 +19,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
 }
