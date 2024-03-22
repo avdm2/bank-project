2568e16627dae1a47ce616213716932a0b6befec
Artem <avdmitriev_2@edu.hse.ru>
2024-03-22 20:31:00 +0300

    [HW5] fix

--- accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
+++ accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
@@ -1,13 +1,11 @@
 package ru.tinkoff.hse.configurations;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
