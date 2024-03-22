cc28db62283f56ed6fe67dca72dbe6306f183ebb
Artem <avdmitriev_2@edu.hse.ru>
2024-03-22 19:45:08 +0300

    [HW5] log

--- accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
+++ accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
@@ -1,12 +1,18 @@
 package ru.tinkoff.hse.configurations;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    public WebSocketConfig() {
+        log.info("WebSocketConfig loaded");
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
