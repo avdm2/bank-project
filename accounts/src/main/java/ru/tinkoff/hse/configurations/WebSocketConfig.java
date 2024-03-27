f9e59880c78dcd851b7333498f2624e47afcb241
Artem <avdmitriev_2@edu.hse.ru>
2024-03-28 02:24:44 +0300

    [HW6] fix

--- accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
+++ accounts/src/main/java/ru/tinkoff/hse/configurations/WebSocketConfig.java
@@ -9,14 +9,13 @@ import org.springframework.web.socket.config.annotation.*;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic/accounts");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws");
     }
 }
