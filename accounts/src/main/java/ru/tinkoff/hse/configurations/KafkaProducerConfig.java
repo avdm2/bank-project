846c8a3867a2b6bc829cf091d341fd225c9f0b4a
Artem <avdmitriev_2@edu.hse.ru>
2024-04-24 19:59:44 +0300

    [HW9] removed jackson import from pom

--- accounts/src/main/java/ru/tinkoff/hse/configurations/KafkaProducerConfig.java
+++ accounts/src/main/java/ru/tinkoff/hse/configurations/KafkaProducerConfig.java
@@ -22,11 +22,11 @@ public class KafkaProducerConfig {
 
     @Bean
     public ProducerFactory<String, KafkaMessage> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
--- accounts/src/main/java/ru/tinkoff/hse/entities/Fee.java
+++ accounts/src/main/java/ru/tinkoff/hse/entities/Fee.java
@@ -2,6 +2,8 @@ package ru.tinkoff.hse.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -20,6 +22,7 @@ import java.util.UUID;
 public class Fee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "fee_id")
     private UUID feeId;
 
